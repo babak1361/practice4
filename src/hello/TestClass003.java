44247d61976e85e9bdaf67d44cf5e5a03d8bfeb6
babak <babak.nikahval.bn@gmail.com>
2022-01-24 11:47:34 -0500

    TestCase003  Commit 3

--- src/hello/TestClass003.java
+++ src/hello/TestClass003.java
@@ -6,3 +6,21 @@ public class TestClass003 {
 
     }
 }
+/*
+Create a new branch called tc003
+
+Create a new class called TestCase003 and make a commit with proper commit message
+
+Add 2 more changes and make 2 more commits
+
+Check out master and Create a class with same name TestCase003 and add different content and commit (to simulate conflicting situation)
+
+And now merge your commits into master branch
+
+BOOM! CONFLICT ! 4 OPTIONS
+
+keep the version in master
+keep the version in tc003
+keep some part of master and some part of tc003
+abort the merge and start over
+ */
